560ebc08ee4c800714f9d854b8c5c36d7c41d5f8
V999TEC <localhost@btconnect.com>
2023-10-05 11:48:09 +0100

    examples

--- download/p.sh
+++ download/p.sh
@@ -4,13 +4,15 @@ if [ "$#" -eq  "0" ]
 	p1="My.properties"
 	p2="PlugE"
 	p3=""
+	p4=""
 else
 	p1="$1"
 	p2="$2"
 	p3="$3"
+	p4="$4"
 fi
 
-task="java -jar plugs.jar $p1 $p2 $p3"
+task="java -jar plugs.jar $p1 $p2 $p3 $p4"
 
 $task
 
